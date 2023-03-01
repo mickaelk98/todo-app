736e07004f9cad39c1302040bbc140e332bf35fb
mickael keita <keitamickael@gmail.com>
2023-03-01 16:52:38 +0100

    feat: add new todo in todos array

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,10 +1,38 @@
+import React, { useState } from "react";
 import { Todo } from "../interface/TodoInterface";
 
-const AddTodo = ({ addTodo }: { addTodo: (todo: Todo) => void }) => {
+interface Props {
+  addTodo: (todo: Todo) => void;
+}
+
+const AddTodo: React.FC<Props> = ({ addTodo }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
+  function addNewTodo(e: React.FormEvent) {
+    e.preventDefault();
+    const todoObjetct: Todo = {
+      _id: crypto.randomUUID(),
+      text: inputValue,
+      done: false,
+      edit: false,
+    };
+    addTodo(todoObjetct);
+    setInputValue("");
+  }
+
   return (
-    <form className="add-task-form">
-      <input type="text" placeholder="Ajouter une tache" />
-      <button>Ajouter</button>
+    <form className="add-task-form" onSubmit={addNewTodo}>
+      <input
+        value={inputValue}
+        onChange={handleChange}
+        type="text"
+        placeholder="Ajouter une tache"
+      />
+      <button type="submit">Ajouter</button>
     </form>
   );
 };
